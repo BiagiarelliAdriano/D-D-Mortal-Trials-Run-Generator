b51d895914b1ea41b090656a7564045c859f98bf
Biagiarelli Adriano <adrianobiagiarelli@gmail.com>
2025-04-23 12:37:30 +0200

    Add frontend interface

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from encounter_generator import generate_encounter
 
 app = Flask(__name__)
+last_result = None
 
-@app.route('/encounter', methods=['GET'])
-def get_encounter():
-    try:
-        encounter_number = int(request.args.get('number'))
-        result = generate_encounter(encounter_number)
-        return jsonify(result)
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    global last_result
+    if request.method == 'POST':
+        # Generate new run
+        run_output = []
+        for i in range(1, 40):
+            run_output.append((i, generate_encounter(i)))
+        last_result = run_output
+        return render_template('index.html', run=last_result)
+    
+    return render_template('index.html', run=last_result)
 
 if __name__ == '__main__':
     app.run(debug=True)
